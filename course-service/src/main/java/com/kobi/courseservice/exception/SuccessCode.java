6f23a6af8ed20b5941444f637cd1fc1ee729a725
Kobi466 <loint.24ite@vku.udn.vn>
2025-09-17 21:58:45 +0700

    refactor

---
+++ course-service/src/main/java/com/kobi/courseservice/exception/SuccessCode.java
@@ -0,0 +1,16 @@
+package com.kobi.courseservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum SuccessCode {
+	UPLOAD_SUCCESS(200, "Upload success", HttpStatus.OK),
+	;
+
+	private final int status;
+	private final String message;
+	private final HttpStatus httpStatus;
+}
